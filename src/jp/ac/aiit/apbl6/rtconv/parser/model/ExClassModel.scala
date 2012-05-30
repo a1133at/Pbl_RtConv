101d1b82f24beb96f45905f155eb85d95b50c1b9
a1148sh <a1148sh@aiit.ac.jp>
2012-05-31 02:52:13 +0900

    抽象構文木からJavaモデルへの変換の実装と専用のデータモデルクラスの作成。 # 構文解析の時点ではClassの継承情報については文字列でしか持ってないので #　ClassModelに直接変換できない。そのため継承情報を文字列で持つクラスを作成した。

---
+++ src/jp/ac/aiit/apbl6/rtconv/parser/model/ExClassModel.scala
@@ -0,0 +1,20 @@
+package jp.ac.aiit.apbl6.rtconv.parser.model
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: HEYA
+ * Date: 12/05/29
+ * Time: 14:58
+ * To change this template use File | Settings | File Templates.
+ */
+
+import jp.ac.aiit.apbl6.rtconv.model._
+
+class ExClassModel(name: String,
+                   modifiers: Array[ModifierModel],
+                   interfaces: Array[InterfaceModel],
+                   members: Array[IMemberModel],
+                   val exClassName : List[String],
+                   val exInterfaces: List[String]) extends ClassModel(name,modifiers,interfaces,members){
+
+}
