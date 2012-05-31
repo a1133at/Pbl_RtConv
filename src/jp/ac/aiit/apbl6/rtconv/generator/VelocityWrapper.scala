aa622bd82770226b09cbe972a0f33c0f7bc69cff
Ryusaburo Tanaka <rew-the-blow@cosmos.ocn.ne.jp>
2012-06-01 01:50:46 +0900

    commit

---
+++ src/jp/ac/aiit/apbl6/rtconv/generator/VelocityWrapper.scala
@@ -0,0 +1,13 @@
+package jp.ac.aiit.apbl6.rtconv.generator
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ryu
+ * Date: 12/05/28
+ * Time: 2:08
+ * To change this template use File | Settings | File Templates.
+ */
+
+class VelocityWrapper {
+
+}
