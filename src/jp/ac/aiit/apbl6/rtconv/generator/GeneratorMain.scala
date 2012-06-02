647d671d7a6aec21fcb20a3bfa05bd4ea35ad86f
Ryusaburo Tanaka <rew-the-blow@cosmos.ocn.ne.jp>
2012-06-02 22:21:29 +0900

    generator commit

--- src/jp/ac/aiit/apbl6/rtconv/generator/GeneratorMain.scala
+++ src/jp/ac/aiit/apbl6/rtconv/generator/GeneratorMain.scala
@@ -1,5 +1,12 @@
 package jp.ac.aiit.apbl6.rtconv.generator
 
+import java.io.PrintWriter
+import java.io.File
+import org.apache.velocity._
+import java.lang.reflect.Modifier
+import reflect.Field
+import jp.ac.aiit.apbl6.rtconv.model._
+
 /**
  * Created with IntelliJ IDEA.
  * User: ryu
@@ -9,5 +16,32 @@ package jp.ac.aiit.apbl6.rtconv.generator
  */
 
 object GeneratorMain {
+  def main(args: Array[String]): Unit = {
+    println("program start")
+
+    // making test data
+//    val import1 = new ImportModel("java.io.PrintWriter1")
+//    val import2 = new ImportModel("java.io.PrintWriter2")
+    val import1 = new ImportModel(" java.io.PrintWriter1")
+    val import2 = new ImportModel("java.io.PrintWriter2")
+
+    val javaModel = new JavaModel(
+      "jp.co.hogehuga.implementation",
+      Array(import1, import2),
+      new InterfaceModel("testInterface", Array(new FieldModel("testField", false, Array(ModifierModel.PUBLIC), new TypeModel("int"))))
+    )
+
+    // use Velocity
+    val velWrapper = new VelocityWrapper(File.separator + "template" + File.separator + "templateJava.vm")
+    velWrapper.put("javaModel", javaModel)
+    val result = velWrapper.merge()
+    System.out.println("expand test data")
+    val pw	= new PrintWriter("output.xml", "UTF-8")
+    pw.print(result)
+    System.out.println("output the data")
+    pw.close()
+
+    println("program end")
+  }
 
 }
--- src/jp/ac/aiit/apbl6/rtconv/generator/VelocityWrapper.scala
+++ src/jp/ac/aiit/apbl6/rtconv/generator/VelocityWrapper.scala
@@ -1,5 +1,16 @@
 package jp.ac.aiit.apbl6.rtconv.generator
 
+import java.io.IOException
+import java.io.StringWriter
+import java.util.Properties
+import org.apache.velocity.Template
+import org.apache.velocity.VelocityContext
+import org.apache.velocity.app.VelocityEngine
+import org.apache.velocity.exception.MethodInvocationException
+import org.apache.velocity.exception.ParseErrorException
+import org.apache.velocity.exception.ResourceNotFoundException
+import org.apache.velocity.runtime.RuntimeConstants
+
 /**
  * Created with IntelliJ IDEA.
  * User: ryu
@@ -8,6 +19,25 @@ package jp.ac.aiit.apbl6.rtconv.generator
  * To change this template use File | Settings | File Templates.
  */
 
-class VelocityWrapper {
+class VelocityWrapper(templeFileName : String) {
 
+  private	val engine		= new VelocityEngine()
+  private val template	= engine.getTemplate(templeFileName);
+  private val context		= new VelocityContext()
+
+  def set = {
+    val props		= new Properties()
+    //		engine.setProperty(VelocityEngine.RUNTIME_LOG_LOGSYSTEM_CLASS, "org.apache.velocity.runtime.log.NullLogSystem")
+    engine.init(props)
+  }
+
+  def put(key: String, value: Any) {
+    context.put(key, value)
+  }
+
+  def merge() = {
+    val sw		= new StringWriter()
+    template.merge(context, sw)
+    sw.toString()
+  }
 }
